9e8ccbf5b19a65e85e798ac0f4b3bfae96b71399
Mariawillow <mani0013@stud.kea.dk>
2025-12-09 16:00:00 +1100

    maria svg

--- src/components/venneforening/HeroVenner.jsx
+++ src/components/venneforening/HeroVenner.jsx
@@ -1,27 +1,52 @@
 import BellevueStriber from "@/components/global/animationer/BellevueStriber";
+import Image from "next/image";
+
 
 const HeroVenner = () => (
   <div>
-    <div className=" max-h-[900px]">
       <BellevueStriber>
-        <div className=" pt-50 pb-20 sm:px-10 md:px-20">
+        <div className=" pt-50 pb-60 sm:px-10 md:px-20">
           <div>
-            <h1>VENNE LOGO</h1>
+            <h1>Venneforening</h1>
           </div>
           <section>
-            <div className="grid grid-cols-3 gap-3 font-bold text-(--roed-600) text-center">
-              <p className="bg-(--gul-600) p-8 rounded-2xl">Gratis Billetter</p>
-              <p className="bg-(--gul-600) p-8 rounded-2xl">
-                Eksklusive arrengementer
-              </p>
-              <p className="bg-(--gul-600) p-8 rounded-2xl ">
-                12 måneders medlemsskab
-              </p>
+            {/* IKONER + GULE TEKSTBOKSE */}
+        <div className="flex flex-col gap-16 items-stretch sm:flex-row sm:justify-between sm:gap-8">
+          {/* PARKERING */}
+          <div className="relative flex-1">
+            <div className="absolute -top-16 left-6">
+              <Image src="ikoner/Billet.svg" alt="" width={100} height={100} />
             </div>
+            <p className="highlighttext bg-(--gul-600) rounded-2xl px-6 py-10 h-full flex items-center justify-center text-center">
+              Gratis billetter
+            </p>
+          </div>
+
+          {/* STOL */}
+          <div className="relative flex-1">
+            <div className="absolute -top-16 left-6">
+              <Image src="/svg/chair.svg" alt="" width={80} height={80} />
+            </div>
+
+            <p className="highlighttext bg-(--gul-600) rounded-2xl px-6 py-10 h-full flex items-center justify-center text-center">
+            Eksklusive arrangementer
+                        </p>
+          </div>
+
+          {/* CATERING */}
+          <div className="relative flex-1">
+            <div className="absolute -top-16 left-6">
+              <Image src="/svg/catering.svg" alt="" width={100} height={100} />
+            </div>
+
+            <p className="highlighttext bg-(--gul-600) rounded-2xl px-6 py-10 h-full flex items-center justify-center text-center">
+            12 måneders medlemskab
+            </p>
+          </div>
+        </div>
           </section>
         </div>
       </BellevueStriber>
-    </div>
   </div>
 );
 
