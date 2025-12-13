e0ec5ced0e0843ab27444aa9e63d20bc973dbccc
Mariawillow <mani0013@stud.kea.dk>
2025-12-13 22:22:54 +1100

    mari fejrnet p

--- src/components/kontakt/Nyhedsbrev.jsx
+++ src/components/kontakt/Nyhedsbrev.jsx
@@ -42,7 +42,7 @@ const Nyhedsbrev = () => {
   return (
     <div className="">
       <section className="pt-20 pb-20">
-        <div className="p-0 md:p-20 md:px-4 mx-auto max-w-xl">
+        <div className="p-0 md:px-4 mx-auto max-w-xl">
           <div className="col-span-2 bg-(--bellevueblaa-600) text-(--beige-600) rounded-0 md:rounded-2xl p-10">
             <div className="w-full mx-auto flex flex-col gap-5 font-light">
             <div className="flex justify-center">
